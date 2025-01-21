a57e34f94b6b8ebdc6b10c3e446844a1bfe1058f
sphelelemendu <sphelelemendu@gmail.com>
2025-01-21 14:46:17 +0200

    Added Navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Button from 'react-bootstrap/Button'
 import { Home } from './Components/Home';
 import { Department } from './Components/Department';
+import { Navigation } from './Components/Navigation';
 
 import { Employee } from './Components/Employee';
 import { BrowserRouter, Router, Route, Routes } from 'react-router-dom';
@@ -12,6 +12,9 @@ function App() {
   return (
     <BrowserRouter>
     <div className="container">
+      <h3 className='m-3 d-flex justify-content-center'>This is a Demo App</h3>
+      <h5 className='m-3 d-flex justify-content-center'>Employee Management Portal</h5>
+        <Navigation/>
         <Routes>
           <Route path='/' Component={Home} exact/>
           <Route path='/Department' Component={Department}/>
