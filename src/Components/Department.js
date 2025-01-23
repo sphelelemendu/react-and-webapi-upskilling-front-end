1bf2b893e93d2fb98063dcce95599ce4e7f5f27d
sphelelemendu <sphelelemendu@gmail.com>
2025-01-23 10:08:18 +0200

    Add the modal component into my Department component screen

--- src/Components/Department.js
+++ src/Components/Department.js
@@ -1,47 +1,57 @@
 import React, { Component } from "react";
-import { Table } from "react-bootstrap";
-
+import { ButtonToolbar, Table } from "react-bootstrap";
+import { Button, ButtonToolBar } from "react-bootstrap";
+import { AddDepModal } from "./AddDepModal";
 export class Department extends Component {
 
     constructor(props) {
         super(props)
-        this.state = { deps: [] }
+        this.state = { deps: [], AddModalShow: false }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.refreshList()
     }
 
     refreshList() {
         fetch('https://localhost:44303/api/department')
-        .then(response=>response.json())
-        .then(data=>{
-            this.setState({deps:data})
-        })
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ deps: data })
+            });
     }
 
     render() {
-        const { deps } = this.state
+        let AddModalClose = () => this.setState({ AddModalShow: false });
+        const { deps } = this.state;
         return (
-            <Table className="mt-4" striped bordered hover size="sm">
-                <thead>
-                    <tr>
-                    <th>Department ID</th>
-                    <th>Department Name</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {deps.map(dep =>
-                        <tr key={dep.DepartmentID}>
-                            <td>{dep.DepartmentID}</td>
-                            <td>{dep.DepartmentName}</td>
+            <div className="container mt-4">
+                <Table className="mt-4" striped bordered hover size="sm">
+                    <thead>
+                        <tr>
+                            <th>Department ID</th>
+                            <th>Department Name</th>
                         </tr>
+                    </thead>
+                    <tbody>
+                        {deps.map(dep =>
+                            <tr key={dep.DepartmentID}>
+                                <td>{dep.DepartmentID}</td>
+                                <td>{dep.DepartmentName}</td>
+                            </tr>
 
-                    )}
+                        )}
 
-                </tbody>
+                    </tbody>
 
-            </Table>
+                </Table>
+                <ButtonToolbar>
+                    <Button variant="primary" onClick={() => this.setState({ AddModalShow: true })
+                    }>Add Department</Button>
+                </ButtonToolbar>
+                <AddDepModal show ={this.state.AddModalShow} onHide ={AddModalClose} />
+            </div>
+
         );
     }
 }
