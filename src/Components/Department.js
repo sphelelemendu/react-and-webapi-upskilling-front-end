dd191316cef073529d5f8c6f5ee806df944d326d
sphelelemendu <sphelelemendu@gmail.com>
2025-01-26 11:03:48 +0200

    Implemented Delete method

--- src/Components/Department.js
+++ src/Components/Department.js
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 import { ButtonToolbar, Table } from "react-bootstrap";
 import { Button, ButtonToolBar } from "react-bootstrap";
 import { AddDepModal } from "./AddDepModal";
+import { EditDepModal } from "./EditDepModal";
 export class Department extends Component {
 
     constructor(props) {
         super(props)
-        this.state = { deps: [], AddModalShow: false }
+        this.state = { deps: [], AddModalShow: false, EditModalShow: false }
     }
 
     componentDidMount() {
@@ -24,9 +25,21 @@ export class Department extends Component {
         this.refreshList();
     }
 
+    deleteDep(depid){
+        if(window.confirm("Are you sure?")){
+            fetch("https://localhost:44303/api/department/"+depid,{
+                method:'Delete',
+                headers: {'Accept':'application/json', 'Content-Type':'application/json'},
+        
+            }
+
+            )}
+    }
+
     render() {
         let AddModalClose = () => this.setState({ AddModalShow: false });
-        const { deps } = this.state;
+        let EditModalClose = () => this.setState({ EditModalShow: false });
+        const { deps,depid,depname } = this.state;
         return (
             <div className="container mt-4">
                 <Table className="mt-4" striped bordered hover size="sm">
@@ -34,6 +47,7 @@ export class Department extends Component {
                         <tr>
                             <th>Department ID</th>
                             <th>Department Name</th>
+                            <th>Option</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -41,6 +55,21 @@ export class Department extends Component {
                             <tr key={dep.DepartmentID}>
                                 <td>{dep.DepartmentID}</td>
                                 <td>{dep.DepartmentName}</td>
+                                <td><ButtonToolbar>
+                                    <Button className="mr-5" variant="info" onClick={()=> this.setState({EditModalShow:true, depid:dep.DepartmentID,depname:dep.DepartmentName})}>
+                                        Edit
+                                    </Button>
+                                    <Button className="ml-2" variant="danger" onClick={()=> this.deleteDep(dep.DepartmentID)}>
+                                        Delete
+                                    </Button>
+                                    <EditDepModal
+                                    show={this.state.EditModalShow}
+                                    onHide={EditModalClose}
+                                    depid = {depid}
+                                    depname = {depname}/>
+
+
+                                    </ButtonToolbar></td>
                             </tr>
 
                         )}
@@ -48,6 +77,8 @@ export class Department extends Component {
                     </tbody>
 
                 </Table>
+
+
                 <ButtonToolbar>
                     <Button variant="primary" onClick={() => this.setState({ AddModalShow: true })
                     }>Add Department</Button>
